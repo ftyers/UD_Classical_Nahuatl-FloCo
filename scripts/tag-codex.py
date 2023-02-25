bb2651ae0de8eb6f54060a0f9f49d0408da556a3
Francis Tyers <ftyers@prompsit.com>
2023-02-25 22:32:37 +0000

    more tagging

--- scripts/tag-codex.py
+++ scripts/tag-codex.py
@@ -24,6 +24,8 @@ def guess(norm):
 		return ('VERB', '_', 'Guessed=Yes')
 	if re.findall('^qui[a-z]+([io]|hu)a$', norm):
 		return ('VERB', '_', 'Guessed=Yes')
+	if re.findall('^tinech', norm):				# tinechitlacoa
+		return ('VERB', '_', 'Guessed=Yes')
 	if re.findall('^nimitz', norm):				# nimitzpantia
 		return ('VERB', '_', 'Guessed=Yes')
 	if re.findall('^mo[a-z]+ya$', norm):
