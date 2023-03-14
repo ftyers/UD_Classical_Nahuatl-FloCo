8bef0e5d158db75ad023012774f2b509db4a6437
Francis Tyers <ftyers@prompsit.com>
2023-03-14 16:15:58 +0000

    some stuff from book08

--- not-to-release/fst/coverage.sh
+++ not-to-release/fst/coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-unknown=$(cat ../../scripts/lexicon.tsv  | cut -f1 | hfst-lookup nci.mor.hfst 2>/dev/null | grep '+?' | wc -l)
-total=$(cat ../../scripts/lexicon.tsv  | cut -f1 | hfst-lookup nci.mor.hfst 2>/dev/null | grep '^$' | wc -l)
+unknown=$(cat ../scripts/lexicon.tsv  | cut -f1 | hfst-lookup nci.mor.hfst 2>/dev/null | grep '+?' | wc -l)
+total=$(cat ../scripts/lexicon.tsv  | cut -f1 | hfst-lookup nci.mor.hfst 2>/dev/null | grep '^$' | wc -l)
 known=$(echo "${total} - ${unknown}" | bc -l)
 coverage=$(echo "${known}/${total}*100" | bc -l | head -c 6)
 
