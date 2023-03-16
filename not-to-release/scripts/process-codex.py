55e64e06d38522a9b424b634f880d8a3480ad38a
Francis Tyers <ftyers@prompsit.com>
2023-03-16 01:14:14 +0000

    more

--- not-to-release/scripts/process-codex.py
+++ not-to-release/scripts/process-codex.py
@@ -6,7 +6,7 @@ def tokenise(s):
 	o = s
 	o = re.sub('([,:.;?!()]+)', ' \g<1> ', o)
 	o = o.replace(" ). ", " ) . ")
-	for etc in ['etc', '&c', 'Etc', 'q. n']:
+	for etc in ['etc', '&c', 'Etc', 'q. n', 'xpo']:
 		o = o.replace(etc + ' .', etc + '.')
 		o = o.replace('. ' + etc, etc)
 	o = re.sub('  *', ' ', o)
@@ -176,6 +176,7 @@ for line in open(sys.argv[1]):
 	line = re.sub('\([0-9]+\)', '', line)
 	# We need to track and replace tokens that end in a full stop
 	line = line.replace('q. n.', '@#@16@#@ @#@17@#@')
+	line = line.replace('xpo.', '@#@18@#@')
 
 	if line.strip() == '¶':
 		current_paragraph += 1
@@ -194,12 +195,15 @@ norm_overrides = {}
 for token in tokens:
 	if token[0] != '¶':
 		# Make this more beautiful
-		if token[0] in ['@#@16@#@', '@#@17@#@']:
+		if token[0] in ['@#@16@#@', '@#@17@#@', '@#@18@#@']:
 			if token[0] == '@#@16@#@':
 				current_sentence.append(('q.', token[1], token[2], token[3]))
 				continue
 			if token[0] == '@#@17@#@':
 				current_sentence.append(('n.', token[1], token[2], token[3]))
+				continue
+			if token[0] == '@#@18@#@':
+				current_sentence.append(('xpo.', token[1], token[2], token[3]))
 				continue
 		else: 
 			current_sentence.append(token)
