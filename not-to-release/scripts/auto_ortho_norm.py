105dce7abc6901b8d0f34962890b50f758263ac1
Robert Pugh <robert.pugh408@gmail.com>
2023-07-28 20:12:53 -0400

    added stuff for automatic normalization

--- not-to-release/scripts/auto_ortho_norm.py
+++ not-to-release/scripts/auto_ortho_norm.py
@@ -1,28 +1,32 @@
-from os import system
+"""
+Run this script whenever you ahve new conllu files and want to re-generate them
+with automatic orthographic normalizations from the seq2seq model. You will
+need to have OpenNMT-py installed (sorry).
+
+"""
 from pathlib import Path
-from sys import argv
 import re
+import requests
+import os
+
+os.system("sh start_onmt_server.sh")
+
 
 word_id_pattern = re.compile("^[0-9]+\t")
 misc_norm_pattern = re.compile("Norm=\*[^\|]+")
-model_path = "../models/normalization_model__step_11878.pt"
-src_filepath = "/tmp/floco_onmt_norm_words.txt"
-output_filepath = "/tmp/floco_onmt_norm_out_{booktitle}.preds"
 
 
-if len(argv) >= 3:
-    USE_CACHED_NORMS = True
-else:
-    USE_CACHED_NORMS = False
-
-
-translate_cmd = (
-    "onmt_translate "
-    "-model {model_path} "
-    "-src {src_filepath} "
-    "-output {output_filepath} "
-    "--replace_unk -verbose"
-)
+def autonorm(tokens):
+	"""
+    Automatically normalize orthography for a word
+    """
+	inp = [{"src": token, "id": i+1} for i, token in enumerate(tokens)]
+	response = requests.post(
+		"http://127.0.0.1:5000/normalizer/translate", 
+		json=inp
+	).json()
+        
+	return [item["tgt"].replace(' ', '') for item in response[0]]
 
 
 def reformat_token_for_onmt(word):
@@ -31,58 +35,45 @@ def reformat_token_for_onmt(word):
 
 def main(path_to_conllu):
     book = path_to_conllu.split("/")[-1].split(".")[0]
-    output_fp = output_filepath.format(booktitle=book)
 
     with open(path_to_conllu) as f:
         conllu_txt = [line.strip("\n") for line in f.readlines()]
         
-    indices_to_norm, word_forms = [], []
-    for i, line in enumerate(conllu_txt):
+    words_to_norm = []
+    for line in conllu_txt:
         if re.search(word_id_pattern, line) and ("Norm=*" in line or "AutoNormed=True" in line):
             word_form = line.split("\t")[1]
-            indices_to_norm.append(i)
-            word_forms.append(word_form)
+            words_to_norm.append(reformat_token_for_onmt(word_form))
+    if not words_to_norm:
+        return []
+    normed = autonorm(words_to_norm)
 
-    with open(src_filepath, "w") as fout:
-        fout.write("\n".join(
-            [reformat_token_for_onmt(w) for w in word_forms]
-        ))
-    
-    #
-    # System call to onmt since its not straightforward to call directly from Python.
-    #
-    
-    if USE_CACHED_NORMS is False or Path.exists(Path(output_fp)) is False:
-        translate(output_fp)
-    
-    with open(output_fp) as f:
-        preds = [w.strip("\n").replace(" ", "") for w in f]
+    return_lines = []
+    for i, word in enumerate(words_to_norm):
+        return_lines.append(f"0\t1\t{word.replace(' ', '')}\t{normed[i]}")
 
-    for i, line in enumerate(conllu_txt):
-        if i in indices_to_norm:
-            pred = preds[indices_to_norm.index(i)]
-            split_line = line.split("\t")
-            if len(pred) >= len(split_line[1]) * 1.5:
-                print(line)
-                break
-            split_line[-1] = re.sub(
-                misc_norm_pattern, f"Norm={pred}|AutoNormed=True", split_line[-1]
-            )
-
-            print("\t".join(split_line))
+    return return_lines
+    
+def uniq_norm_list(norm_list):
+    seen = set([])
+    new_norm_list = []
+    for line in norm_list:
+        _, _, word, _ = line.split("\t")
+        if word in seen:
+            continue
         else:
-            print(line)
-
-def translate(output_fpath):
-    system(translate_cmd.format(
-        model_path=model_path, 
-        src_filepath=src_filepath, 
-        output_filepath=output_fpath
-        )
-    )
+            new_norm_list.append(line)
+            seen.update({word})
+    return new_norm_list
+    
 
 if __name__ == "__main__":
-    path_to_conllu = argv[1]
-    main(path_to_conllu)
+    import glob
+    new_norm_entries = []
+    for book in glob.glob("../conllu/*conllu"):
+        new_norm_entries.extend(main(book))
+    
+    print("\n".join(uniq_norm_list(new_norm_entries)))
+
 
 
---
+++ not-to-release/scripts/start_onmt_server.sh
@@ -0,0 +1,1 @@
+python onmt_server.py --ip 0.0.0.0 --port 5000 --url_root "/normalizer" --config "./available_models/conf.json"
