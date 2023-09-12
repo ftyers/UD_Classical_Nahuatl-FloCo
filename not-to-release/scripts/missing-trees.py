bb71546363a3d745fdc6f7e1eab4ae82f65368a0
Francis Tyers <ftyers@prompsit.com>
2023-09-12 16:07:49 +0100

    add missing trees script

---
+++ not-to-release/scripts/missing-trees.py
@@ -0,0 +1,41 @@
+import sys
+
+left = {}
+right = {}
+
+for bloc in open(sys.argv[1]).read().split('\n\n'):
+	sent_id = '_'	
+	tree = {}
+	empty = {}
+	if bloc.strip() == '':
+		continue
+	for line in bloc.split('\n'):
+#		print('@@@@', line,file=sys.stderr)
+		if line.count('sent_id') > 0:
+			sent_id = line.split('=')[1].strip()
+
+	left[sent_id] = bloc
+
+for bloc in open(sys.argv[2]).read().split('\n\n'):
+	sent_id = '_'	
+	tree = {}
+	empty = {}
+	if bloc.strip() == '':
+		continue
+	for line in bloc.split('\n'):
+#		print('@@@@', line,file=sys.stderr)
+		if line.count('sent_id') > 0:
+			sent_id = line.split('=')[1].strip()
+			break
+
+	right[sent_id] = bloc 
+
+left_keys = set(list(left.keys()))
+right_keys = set(list(right.keys()))
+
+missing = list(left_keys - right_keys)
+missing.sort(key=lambda x : int(x.split(':')[1]))
+
+for sent_id in missing:
+	print(left[sent_id])
+	print()
